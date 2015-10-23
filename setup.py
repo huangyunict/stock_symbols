9852e0bbef0238ff318c9167f8c03525c209f028
Yun Huang <huangyunict@gmail.com>
2015-10-22 23:11:16 -0500

    add setup cfg

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='stock_symbols',
+    version='0.5.0',
+    packages=['stock_symbols'],
+    author='Yun Huang',
+    author_email='huangyunict@gmail.com',
+    description='Retrieves list of all symbols present in SP500, NASDAQ, AMEX and NYSE',
+    long_description=open('README.md').read(),
+    url='https://github.com/huangyunict/stock_symbols',
+    download_url='http://pypi.python.org/pypi/stock_symbols',
+    keywords='stocks stockmarket yahoo finance SP500 NASDAQ AMEX NYSE'.split(),
+    license='GNU LGPLv2+',
+    install_requires=[
+        "beautifulsoup4 >= 4.2.1"
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Investment',
+    ]
+)
