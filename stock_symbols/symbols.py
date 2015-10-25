275294b842c1c80187456762dee4f77d0aaf9652
Yun Huang <huangyunict@gmail.com>
2015-10-25 09:50:22 -0500

    add comments to functions

--- stock_symbols/symbols.py
+++ stock_symbols/symbols.py
@@ -1,8 +1,4 @@
-import pprint
-import sys
-
 from bs4 import BeautifulSoup
-
 from stock_symbols.symbol_helper import *
 
 
@@ -11,18 +7,26 @@ __work_dir = _get_default_work_dir()
 
 #   set working directory
 def set_work_dir(work_dir=""):
+    """Set working directory, used in following functions.
+    :param work_dir: working directory. If passed in None or empty, using default working directory.
+    """
+    global __work_dir
     if not work_dir:
         __work_dir = _get_default_work_dir()
 
-#   get working directory
+
 def get_work_dir():
+    """Get working directory.
+    :return: working directory.
+    """
     return __work_dir
+
 
 #   pass in working path to avoid potential permission error
 def get_sp500_symbols():
-    page_html = wiki_html('List_of_S%26P_500_companies', os.path.join(get_work_dir(), 'SP500.html'))
+    page_html = wiki_html("List_of_S%26P_500_companies", os.path.join(get_work_dir(), "SP500.html"))
     wiki_soup = BeautifulSoup(page_html, "html.parser")
-    symbol_table = wiki_soup.find(attrs={'class': 'wikitable sortable'})
+    symbol_table = wiki_soup.find(attrs={"class": "wikitable sortable"})
 
     symbol_data_list = list()
 
@@ -31,16 +35,16 @@ def get_sp500_symbols():
         symbol_raw_data = symbol.find_all("td")
         td_count = 0
         for symbol_data in symbol_raw_data:
-            if(td_count == 0):
-                symbol_data_content['symbol'] = symbol_data.text.encode('utf-8')
-            elif(td_count == 1):
-                symbol_data_content['company'] = symbol_data.text.encode('utf-8')
-            elif(td_count == 3):
-                symbol_data_content['sector'] = symbol_data.text.encode('utf-8')
-            elif(td_count == 4):
-                symbol_data_content['industry'] = symbol_data.text.encode('utf-8')
-            elif(td_count == 5):
-                symbol_data_content['headquaters'] = symbol_data.text.encode('utf-8')
+            if td_count == 0:
+                symbol_data_content["symbol"] = symbol_data.text.encode("utf-8")
+            elif td_count == 1:
+                symbol_data_content["company"] = symbol_data.text.encode("utf-8")
+            elif td_count == 3:
+                symbol_data_content["sector"] = symbol_data.text.encode("utf-8")
+            elif td_count == 4:
+                symbol_data_content["industry"] = symbol_data.text.encode("utf-8")
+            elif td_count == 5:
+                symbol_data_content["headquaters"] = symbol_data.text.encode("utf-8")
 
             td_count += 1
 
@@ -50,30 +54,44 @@ def get_sp500_symbols():
 
 
 def get_nyse_symbols():
+    """Get symbols from New York Stock Exchange market.
+    :return: Symbol list from NYSE market.
+    """
     return _get_exchange_data("NYSE")
 
 
 def get_amex_symbols():
+    """Get symbols from Amercan American Stock Exchange market.
+    :return: Symbol list from AMEX market.
+    """
     return _get_exchange_data("AMEX")
 
 
 def get_nasdaq_symbols():
+    """Get symbols from NASDAQ market.
+    :return: Symbol list from NASDAQ market.
+    """
     return _get_exchange_data("NASDAQ")
 
 
-def _get_exchange_data(exchange):
-    url = get_exchange_url(exchange)
-    file_path = os.path.join(get_work_dir, exchange)
+def _get_exchange_data(market):
+    """Get market symbols from NASDAQ website,
+    :param market: exchange market, possible values are: "AMEX", "NASDAQ", "NYSE".
+    :return: symbol list.
+    """
+    url = get_exchange_url(market)
+    file_path = os.path.join(get_work_dir(), market)
     if is_cached(file_path):
         with open(file_path, "r") as cached_file:
             symbol_data = cached_file.read()
     else:
-      symbol_data = fetch_file(url)
-      save_file(file_path, symbol_data)
-
-    return get_symbol_list(symbol_data, exchange)
+        symbol_data = fetch_file(url)
+        save_file(file_path, symbol_data)
+    return get_symbol_list(symbol_data, market)
 
 
 def _get_default_work_dir():
+    """Get default work directory, default to the package install path
+    :return: default work directory.
+    """
     return os.path.dirname(stock_symbols.__file__)
-
